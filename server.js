5af4060e1590ed5ae99eb5a7ac33306c8ea1d70f
Nadeem-h <74771997+Nadeem-h@users.noreply.github.com>
2025-02-03 15:00:05 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, World! This app is running on GCP GKE 🚀");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
