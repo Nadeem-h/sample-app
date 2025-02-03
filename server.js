07fffd60c2071fee4f7bd65620d4e0d1fa88f494
Nadeem-h <nadeemhassan1@yahoo.com>
2025-02-03 14:13:05 +0000

    Fix Dockerfile path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! This app is running on GCP GKE 🚀");
+    res.send("Hello, World! This app is running on GCP GKE");
 });
 
 app.listen(port, () => {
