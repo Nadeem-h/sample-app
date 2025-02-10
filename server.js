0c746dfe1dd7f98928f124355dc3367365e131ce
Nadeem-h <nadeemhassan1@yahoo.com>
2025-02-10 13:10:45 +0000

    Changed server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! This app is running on GCP GKE Done!");
+    res.send("Hello, World! This app is running on GCP GKE Done now!");
 });
 
 app.listen(port, () => {
